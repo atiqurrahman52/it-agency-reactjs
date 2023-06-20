5e5eaafe89063885728e4fd5f293a4e38cfabd93
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-06-20 10:48:09 +0600

    updated

--- src/data/share/testimonialData.jsx
+++ src/data/share/testimonialData.jsx
@@ -1,49 +1,52 @@
+import sebastian from '../../assets/images/testimonial/sebastian.webp';
+import nicholas from '../../assets/images/testimonial/nicholas.webp';
+import jonas from '../../assets/images/testimonial/jonas.webp'
 export const testimonialData = [
     {
         id:0,
-        UserImg:'/src/assets/images/testimonial/sebastian.webp',
+        UserImg:sebastian,
         userName:'Sebastian',
         designation:'CEO Google',
         comment:'Massa enim libero dictumst consectetur in convallis. Lobortis cursus mi a magna ullamcorper consectetur.'
     },
     {
         id:1,
-        UserImg:'/src/assets/images/testimonial/nicholas.webp',
+        UserImg:nicholas,
         userName:'Nicholas',
         designation:'CEO Amazon',
         comment:'Massa enim libero dictumst consectetur in convallis. Lobortis cursus mi a magna ullamcorper consectetur.'
     },
     {
         id:2,
-        UserImg:'/src/assets/images/testimonial/sebastian.webp',
+        UserImg:sebastian,
         userName:'Sebastian',
         designation:'CEO Google',
         comment:'Massa enim libero dictumst consectetur in convallis. Lobortis cursus mi a magna ullamcorper consectetur.'
     },
     {
         id:3,
-        UserImg:'/src/assets/images/testimonial/jonas.webp',
+        UserImg:jonas,
         userName:'Jonas',
         designation:'CEO BYSL',
         comment:'Massa enim libero dictumst consectetur in convallis. Lobortis cursus mi a magna ullamcorper consectetur.'
     },
     {
         id:4,
-        UserImg:'/src/assets/images/testimonial/sebastian.webp',
+        UserImg:sebastian,
         userName:'Sebastian',
         designation:'CEO Microsoft',
         comment:'Massa enim libero dictumst consectetur in convallis. Lobortis cursus mi a magna ullamcorper consectetur.'
     },
     {
         id:5,
-        UserImg:'/src/assets/images/testimonial/nicholas.webp',
+        UserImg:nicholas,
         userName:'Nicholas',
         designation:'CEO Amazon',
         comment:'Massa enim libero dictumst consectetur in convallis. Lobortis cursus mi a magna ullamcorper consectetur.'
     },
     {
         id:6,
-        UserImg:'/src/assets/images/testimonial/jonas.webp',
+        UserImg:jonas,
         userName:'Jonas',
         designation:'CEO BYSL',
         comment:'Massa enim libero dictumst consectetur in convallis. Lobortis cursus mi a magna ullamcorper consectetur.'
