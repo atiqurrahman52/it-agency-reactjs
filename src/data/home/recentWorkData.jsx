6efc70347420f3d6dd76bbe15e3104c777bae7fc
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-06-20 10:41:11 +0600

    updated

--- src/data/home/recentWorkData.jsx
+++ src/data/home/recentWorkData.jsx
@@ -1,57 +1,59 @@
-import video1 from '../../assets/video/business.mp4'
+import video1 from "../../assets/video/business.mp4";
+import video2 from "../../assets/video/tech.mp4";
+
 export const recentWorkData = [
   {
     id: 0,
     video: video1,
-    title: "Business Automation & Integration ONE E",
+    title: "Business Automation & Integration",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 1,
-    video: "/assets/video/tech.mp4",
+    video: video2,
     title: "Top notch quality web application",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 2,
-    video: "/assets/video/business.mp4",
+    video: video1,
     title: "Top notch quality web application",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 3,
-    video: "/assets/video/tech.mp4",
+    video: video2,
     title: "Top notch quality web application",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 4,
-    video: "/assets/video/business.mp4",
+    video: video1,
     title: "Business Automation & Integration",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 5,
-    video: "/assets/video/tech.mp4",
+    video: video2,
     title: "Top notch quality web application",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 6,
-    video: "/assets/video/business.mp4",
+    video: video1,
     title: "Business Automation & Integration",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 7,
-    video: "/assets/video/tech.mp4",
+    video: video2,
     title: "Top notch quality web application",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
