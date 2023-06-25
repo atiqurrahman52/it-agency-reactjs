51f875a54782a78052956ea7acac988860c9b73f
atiqurrahman52 <atiqur.rahman@intelli.global>
2023-06-25 10:24:18 +0600

    updated

--- src/data/home/recentWorkData.jsx
+++ src/data/home/recentWorkData.jsx
@@ -5,56 +5,56 @@ export const recentWorkData = [
   {
     id: 0,
     video: video1,
-    title: "Business Automation & Integration",
+    title: "Business Automation & Integration 1",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 1,
     video: video2,
-    title: "Top notch quality web application",
+    title: "Top notch quality web application 2",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 2,
     video: video1,
-    title: "Top notch quality web application",
+    title: "Top notch quality web application 3",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 3,
     video: video2,
-    title: "Top notch quality web application",
+    title: "Top notch quality web application 4",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 4,
     video: video1,
-    title: "Business Automation & Integration",
+    title: "Business Automation & Integration 5",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 5,
     video: video2,
-    title: "Top notch quality web application",
+    title: "Top notch quality web application 6",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 6,
     video: video1,
-    title: "Business Automation & Integration",
+    title: "Business Automation & Integration 7",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
   {
     id: 7,
     video: video2,
-    title: "Top notch quality web application",
+    title: "Top notch quality web application 8",
     description:
       "21 Job Interview Tips: How To Make a Great Impression.Our mission is to create the world&rsquo;s most sustainable healthcare company by creating high-quality healthcare products in iconic, sustainable packaging.",
   },
